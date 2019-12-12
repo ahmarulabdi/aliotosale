b161a8211b6bce5ef36d423dd9481e33229c0768
ahmarulabdi <rndmjck@gmai.com>
2019-12-13 00:36:13 +0700

    inialize spring file from spring.io

---
+++ src/main/java/com/abdi/aliotosale/AliotosaleApplication.java
@@ -0,0 +1,13 @@
+package com.abdi.aliotosale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliotosaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliotosaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdi/aliotosale/AliotosaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdi.aliotosale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliotosaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
