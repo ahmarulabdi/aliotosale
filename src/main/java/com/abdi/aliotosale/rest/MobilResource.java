c94cfff6af93869de07331e7738199fc3ce681fd
Ahmarul Abdi <rndmjck@gmail.com>
2019-12-13 23:27:47 +0700

    fix requirement rest api without frendly response

--- src/main/java/com/abdi/aliotosale/rest/MobilResource.java
+++ src/main/java/com/abdi/aliotosale/rest/MobilResource.java
@@ -1,12 +1,9 @@
 package com.abdi.aliotosale.rest;
 
 import com.abdi.aliotosale.models.Mobil;
-import com.abdi.aliotosale.repository.MobilRepository;
 import com.abdi.aliotosale.service.MobilService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -17,9 +14,36 @@ public class MobilResource {
     @Autowired
     private MobilService mobilService;
 
-    @GetMapping("/")
+    @GetMapping("")
     public List<Mobil> getAllMobil() {
         return mobilService.getAllMobil();
     }
+
+    @GetMapping("/{merkId}")
+    public List<Mobil> getMobilByMerkId(@PathVariable(name = "merkId") Long merkId) {
+        return mobilService.getMobilByMerkId(merkId);
+    }
+
+    @GetMapping("/{merkId}/{typeId}")
+    public List<Mobil> getMobilByMerkIdTypeId(@PathVariable(name = "merkId") Long merkId, @PathVariable(name = "typeId") Long typeId) {
+        return mobilService.getMobilByMerkIdTypeId(merkId,typeId);
+    }
+
+    @PostMapping("/{merkId}/{typeId}")
+    public Mobil addMobilByMerkIdTypeId(@PathVariable Long merkId, @PathVariable Long typeId, @RequestBody Mobil mobil) {
+        return mobilService.addMobilByMerkIdTypeId(merkId,typeId,mobil);
+    }
+
+    @PutMapping("/{merkId}/{typeId}")
+    public List<Mobil> updateMobilByMerkIdTypeId(@PathVariable Long merkId, @PathVariable Long typeId, @RequestBody Mobil mobil) {
+        return mobilService.updateMobilByMerkIdTypeId(merkId, typeId,mobil);
+    }
+
+    @DeleteMapping("/{merkId}/{typeId}")
+    public Boolean deleteMobilByMerkIdTypeId(@PathVariable Long merkId, @PathVariable Long typeId, @RequestBody Mobil mobil) {
+        return mobilService.deleteMobilByMerkIdTypeId(merkId, typeId,mobil);
+    }
+
+
 
 }
--- src/main/java/com/abdi/aliotosale/service/MobilService.java
+++ src/main/java/com/abdi/aliotosale/service/MobilService.java
@@ -13,12 +13,13 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MobilService {
     @Autowired
     private MobilRepository mobilRepository;
-
+    @Autowired
     private EntityManager entityManager;
 
     public List<Mobil> getAllMobil() {
@@ -29,11 +30,11 @@ public class MobilService {
         return mobilRepository.getOne(id);
     }
 
-    public List<Mobil> getMobilByMerkId(Long merkId){
+    public List<Mobil> getMobilByMerkId(Long merkId) {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Mobil> criteriaQuery = criteriaBuilder.createQuery(Mobil.class);
-
         Root<Mobil> mobilRoot = criteriaQuery.from(Mobil.class);
+
         Predicate predicateMerkId = criteriaBuilder.equal(mobilRoot.get("merkId"), merkId);
         criteriaQuery.where(predicateMerkId);
 
@@ -41,6 +42,90 @@ public class MobilService {
         return typedQuery.getResultList();
     }
 
+    public List<Mobil> getMobilByMerkIdTypeId(Long merkId, Long typeId) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Mobil> criteriaQuery = criteriaBuilder.createQuery(Mobil.class);
+        Root<Mobil> mobilRoot = criteriaQuery.from(Mobil.class);
+
+        Predicate predicateMerkId = criteriaBuilder.equal(mobilRoot.get("merkId"), merkId);
+        Predicate predicateTypeId = criteriaBuilder.equal(mobilRoot.get("typeId"), typeId);
+        criteriaQuery.where(predicateMerkId, predicateTypeId);
+
+        TypedQuery<Mobil> typedQuery = entityManager.createQuery(criteriaQuery);
+        return typedQuery.getResultList();
+    }
+
+    public Mobil addMobilByMerkIdTypeId(Long merkId, Long typeId, Mobil mobil) {
+        Mobil newMobil = new Mobil();
+
+        newMobil.setNomorKerangka(mobil.getNomorKerangka());
+        newMobil.setSeriWilayah(mobil.getSeriWilayah());
+        newMobil.setNomor(mobil.getNomor());
+        newMobil.setSeri(mobil.getSeri());
+        newMobil.setMerkId(merkId);
+        newMobil.setTypeId(typeId);
+        newMobil.setTahun(mobil.getTahun());
+
+        return mobilRepository.save(newMobil);
+    }
+
+    public List<Mobil> updateMobilByMerkIdTypeId(Long merkId, Long typeId, Mobil mobil) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Mobil> criteriaQuery = criteriaBuilder.createQuery(Mobil.class);
+        Root<Mobil> mobilRoot = criteriaQuery.from(Mobil.class);
+
+        Predicate predicateMerkId = criteriaBuilder.equal(mobilRoot.get("merkId"), merkId);
+        Predicate predicateTypeId = criteriaBuilder.equal(mobilRoot.get("typeId"), typeId);
+        criteriaQuery.where(predicateMerkId, predicateTypeId);
+
+        TypedQuery<Mobil> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Mobil> mobils = typedQuery.getResultList();
+
+        for (Mobil m : mobils
+        ) {
+            m.setNomorKerangka(mobil.getNomorKerangka());
+            m.setSeriWilayah(mobil.getSeriWilayah());
+            m.setNomor(mobil.getNomor());
+            m.setSeri(mobil.getSeri());
+            m.setTahun(mobil.getTahun());
+        }
+
+        return mobilRepository.saveAll(mobils);
+    }
+
+
+    public Boolean deleteMobilByMerkIdTypeId(Long merkId, Long typeId, Mobil mobil) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Mobil> criteriaQuery = criteriaBuilder.createQuery(Mobil.class);
+        Root<Mobil> mobilRoot = criteriaQuery.from(Mobil.class);
+
+        Predicate predicateMerkId = criteriaBuilder.equal(mobilRoot.get("merkId"), merkId);
+        Predicate predicateTypeId = criteriaBuilder.equal(mobilRoot.get("typeId"), typeId);
+        criteriaQuery.where(predicateMerkId, predicateTypeId);
+
+        TypedQuery<Mobil> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Mobil> mobils = typedQuery.getResultList();
+
+        if(mobils.size() > 0){
+            for (Mobil m : mobils
+            ) {
+                m.setNomorKerangka(mobil.getNomorKerangka());
+                m.setSeriWilayah(mobil.getSeriWilayah());
+                m.setNomor(mobil.getNomor());
+                m.setSeri(mobil.getSeri());
+                m.setTahun(mobil.getTahun());
+            }
+
+            List<Long> listId = mobils.stream().map(m -> m.getId()).collect(Collectors.toList());
+
+            mobilRepository.deleteAll(mobils);
+
+            return mobilRepository.findAllById(listId).size() == 0;
+        }else{
+            return false;
+        }
+
+    }
 
 
 }
