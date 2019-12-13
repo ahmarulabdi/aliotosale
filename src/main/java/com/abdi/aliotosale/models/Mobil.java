3ec5f2e5be72f3753dd97b1136c0cf74b3e116d6
Ahmarul Abdi <rndmjck@gmail.com>
2019-12-13 09:34:07 +0700

    udpate model

--- src/main/java/com/abdi/aliotosale/models/Mobil.java
+++ src/main/java/com/abdi/aliotosale/models/Mobil.java
@@ -29,7 +29,7 @@ public class Mobil implements Serializable {
     @NotBlank
     private String seriWilayah;
     @NotBlank
-    private Integer Nomor;
+    private Integer nomor;
     @NotBlank
     private String seri;
     @NotBlank
@@ -48,4 +48,116 @@ public class Mobil implements Serializable {
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate
     private Date updatedAt;
+
+    public Mobil(
+            @NotBlank Long nomorKerangka,
+            @NotBlank String seriWilayah,
+            @NotBlank Integer nomor,
+            @NotBlank String seri,
+            @NotBlank String merk,
+            @NotBlank String tipe,
+            @NotBlank Year tahun
+            ){
+        this.nomorKerangka = nomorKerangka;
+        this.seriWilayah = seriWilayah;
+        this.nomor = nomor;
+        this.seri = seri;
+        this.merk = merk;
+        this.tipe = tipe;
+        this.tahun = tahun;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getNomorKerangka() {
+        return nomorKerangka;
+    }
+
+    public void setNomorKerangka(Long nomorKerangka) {
+        this.nomorKerangka = nomorKerangka;
+    }
+
+    public String getSeriWilayah() {
+        return seriWilayah;
+    }
+
+    public void setSeriWilayah(String seriWilayah) {
+        this.seriWilayah = seriWilayah;
+    }
+
+    public Integer getNomor() {
+        return nomor;
+    }
+
+    public void setNomor(Integer nomor) {
+        this.nomor = nomor;
+    }
+
+    public String getSeri() {
+        return seri;
+    }
+
+    public void setSeri(String seri) {
+        this.seri = seri;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+
+    public void setTipe(String tipe) {
+        this.tipe = tipe;
+    }
+
+    public Year getTahun() {
+        return tahun;
+    }
+
+    public void setTahun(Year tahun) {
+        this.tahun = tahun;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Mobil{" +
+                "id="+ id +
+                "nomorKerangka="+ nomorKerangka +
+                "seriWilayah="+ seriWilayah +
+                "nomor="+ nomor +
+                "seri="+ seri +
+                "merk="+ merk +
+                "tipe="+ tipe+
+                "tahun="+tahun+
+                "}";
+    }
 }
