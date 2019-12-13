97dd9ff6832ae61eb17b959cc742fad7dcea793b
Ahmarul Abdi <rndmjck@gmail.com>
2019-12-13 21:57:25 +0700

    adding getAll and getById

--- src/main/java/com/abdi/aliotosale/rest/TypeResource.java
+++ src/main/java/com/abdi/aliotosale/rest/TypeResource.java
@@ -1,14 +1,30 @@
 package com.abdi.aliotosale.rest;
 
 
+import com.abdi.aliotosale.models.Type;
 import com.abdi.aliotosale.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/type")
 public class TypeResource {
     @Autowired
     private TypeService typeService;
+
+    @GetMapping("")
+    public List<Type> getAllType() {
+        return typeService.getAllType();
+    }
+
+    @GetMapping("/{id}")
+    public Type getTypeById(@PathVariable(value = "id") Long id) {
+        return typeService.getTypeById(id);
+    }
+
 }
--- src/main/java/com/abdi/aliotosale/service/TypeService.java
+++ src/main/java/com/abdi/aliotosale/service/TypeService.java
@@ -1,12 +1,26 @@
 package com.abdi.aliotosale.service;
 
 
+import com.abdi.aliotosale.exception.ResourceNotFoundException;
+import com.abdi.aliotosale.models.Type;
 import com.abdi.aliotosale.repository.TypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TypeService {
     @Autowired
     private TypeRepository typeRepository;
+
+    public List<Type> getAllType(){
+        return typeRepository.findAll();
+    }
+
+    public Type getTypeById(Long id){
+        return typeRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Type","ID", id)
+        );
+    }
 }
