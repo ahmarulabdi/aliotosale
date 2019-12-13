63298c2b6133392aa19fac4f92e120d3da7a0686
Ahmarul Abdi <rndmjck@gmail.com>
2019-12-13 20:13:45 +0700

    fix bug

--- src/main/java/com/abdi/aliotosale/exception/ResourceNotFoundException.java
+++ src/main/java/com/abdi/aliotosale/exception/ResourceNotFoundException.java
@@ -9,12 +9,8 @@ public class ResourceNotFoundException extends RuntimeException {
     private String fieldName;
     private Object fieldValue;
 
-    public ResourceNotFoundException(
-            String resourceName,
-            String fieldName,
-            Object fieldValue
-    ) {
-        super(String.format("% not found with %s : '%s'", resourceName, fieldName, fieldValue));
+    public ResourceNotFoundException( String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
