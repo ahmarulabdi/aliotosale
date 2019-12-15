b29e005bc1924848756d8e5c9739234f5a7cf414
Ahmarul Abdi <rndmjck@gmail.com>
2019-12-15 16:14:13 +0700

    update type resource and service

--- src/main/java/com/abdi/aliotosale/rest/TypeResource.java
+++ src/main/java/com/abdi/aliotosale/rest/TypeResource.java
@@ -4,13 +4,11 @@ package com.abdi.aliotosale.rest;
 import com.abdi.aliotosale.models.Type;
 import com.abdi.aliotosale.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("api/type")
 public class TypeResource {
@@ -22,9 +20,19 @@ public class TypeResource {
         return typeService.getAllType();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     public Type getTypeById(@PathVariable(value = "id") Long id) {
         return typeService.getTypeById(id);
+    }
+
+    @PutMapping("{id}")
+    public Type updateTypeById(@PathVariable(value = "id") Long id, @RequestBody Type type){
+        return typeService.updateTypeById(id, type);
+    }
+
+    @DeleteMapping("{id}")
+    public Boolean deleteTypeById(@PathVariable(value = "id") Long id) {
+        return typeService.deleteTypeById(id);
     }
 
 }
--- src/main/java/com/abdi/aliotosale/service/TypeService.java
+++ src/main/java/com/abdi/aliotosale/service/TypeService.java
@@ -14,13 +14,27 @@ public class TypeService {
     @Autowired
     private TypeRepository typeRepository;
 
-    public List<Type> getAllType(){
+    public List<Type> getAllType() {
         return typeRepository.findAll();
     }
 
-    public Type getTypeById(Long id){
+    public Type getTypeById(Long id) {
         return typeRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("Type","ID", id)
+                () -> new ResourceNotFoundException("Type", "ID", id)
         );
+    }
+
+    public Type updateTypeById(Long id, Type type) {
+        Type udpatedType = typeRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Type", "ID", id)
+        );
+
+        udpatedType.setNama(type.getNama());
+        return typeRepository.save(udpatedType);
+    }
+
+    public Boolean deleteTypeById(Long id) {
+        typeRepository.deleteById(id);
+        return typeRepository.findById(id).isEmpty();
     }
 }
