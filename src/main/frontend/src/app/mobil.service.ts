b7f50e288bdf45821611377ea44c25ea76983a71
Ahmarul Abdi <rndmjck@gmail.com>
2019-12-16 18:51:14 +0700

    update service and clean code

--- src/main/frontend/src/app/mobil.service.ts
+++ src/main/frontend/src/app/mobil.service.ts
@@ -13,24 +13,32 @@ export class MobilService {
   private mobilUrl = 'http://localhost:8080/api/mobil';
 
   constructor(
-    protected http: HttpClient
+    protected http: HttpClient,
   ) {
   }
 
   getAllMobil(): Observable<EntityArrayResponseMobil> {
-    return this.http.get<Mobil[]>(this.mobilUrl, { observe: 'response'});
+    return this.http.get<Mobil[]>(this.mobilUrl, {observe: 'response'});
+  }
+
+  getMobilByBrand(brand: string): Observable<EntityArrayResponseMobil> {
+    return this.http.get<Mobil[]>(`${this.mobilUrl}/${brand}/merk`, {observe: 'response'});
+  }
+
+  getMobilByTypeNama(typeNama: string): Observable<EntityArrayResponseMobil> {
+    return this.http.get<Mobil[]>(`${this.mobilUrl}/${typeNama}/type`, {observe: 'response'});
   }
 
   addMobilByMerkIdTypeId(mobil: Mobil): Observable<EntityResponseMobil> {
-    return this.http.post<Mobil>(`${this.mobilUrl}/${mobil.merkId}/${mobil.typeId}`,mobil,{ observe: 'response'})
+    return this.http.post<Mobil>(`${this.mobilUrl}/${mobil.merkId}/${mobil.typeId}`, mobil, {observe: 'response'})
   }
 
-  updateMobilById(id:number, mobil: Mobil): Observable<EntityResponseMobil> {
-    return this.http.put<Mobil>(`${this.mobilUrl}/${id}`,mobil,{ observe: 'response'})
+  updateMobilById(id: number, mobil: Mobil): Observable<EntityResponseMobil> {
+    return this.http.put<Mobil>(`${this.mobilUrl}/${id}`, mobil, {observe: 'response'})
   }
 
-  deleteMobilById(id:number): Observable<HttpResponse<Boolean>> {
-    return this.http.delete<Boolean>(`${this.mobilUrl}/${id}`,{ observe: 'response'})
+  deleteMobilById(id: number): Observable<HttpResponse<Boolean>> {
+    return this.http.delete<Boolean>(`${this.mobilUrl}/${id}`, {observe: 'response'})
   }
 
 
